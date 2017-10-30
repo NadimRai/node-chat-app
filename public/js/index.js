99081afed2e26f0292dff2e0aecb5beb18f9f41d
NadimRai <rai_nadim@yahoo.com>
2017-10-31 02:20:25 +0800

    added moment js to display timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -11,9 +11,10 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function(message){
-    console.log('New Message from: ', message);
+    var formattedTime = moment(message.createAt).format('h:mm a');
+
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 })
@@ -26,10 +27,12 @@ socket.on('newMessage', function(message){
 // })
 
 socket.on('newLocationMessage', function(message){
+    var formattedTime = moment(message.createAt).format('h:mm a');
+
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
