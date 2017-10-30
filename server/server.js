7eabe35f4ee0c12ce05dfcc03b76b0109501b149
NadimRai <rai_nadim@yahoo.com>
2017-10-31 00:42:49 +0800

    added style to chat page

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) =>{
     socket.on('createMessage', (message, callback) =>{
         console.log('createMessage ', message);
         io.emit('newMessage',generateMessage(message.from,message.text));
-        callback('this is from the server');
+        callback();
     })
     //listener of geolocation
     socket.on('createLocationMessage', (coords) =>{
