cb10a9af16ae8a8d0607b6293e86e7920dac60cd
NadimRai <rai_nadim@yahoo.com>
2017-10-31 20:13:42 +0800

    added validation for joining chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
 var socket = io();
 
-function scrollToBottom(){
+function scrollToBottom() {
     //Selectors
     var messages = jQuery('#messages');
     var newMessage = messages.children('li:last-child');
@@ -12,28 +12,35 @@ function scrollToBottom(){
     var newMessageHeight = newMessage.innerHeight();
     var lastMessageHeight = newMessage.prev().innerHeight();
 
-    if (clientHeight + scrollTop +newMessageHeight + lastMessageHeight >= scrollHeight){
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
         console.log('Should scroll');
     }
 }
 
 socket.on('connect', function () {
-    console.log('Connected to server');
+    var params = jQuery.deparam(window.location.search);
 
-   
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No error');
+        }
+    });
 });
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message){
+socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     var template = jQuery('#message-template').html();
     var html = Mustache.render(template, {
-       text: message.text,
-       from: message.from,
-       createAt: formattedTime
+        text: message.text,
+        from: message.from,
+        createAt: formattedTime
     });
 
     jQuery('#messages').append(html);
@@ -41,48 +48,48 @@ socket.on('newMessage', function(message){
 })
 
 
-socket.on('newLocationMessage', function(message){
+socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createAt).format('h:mm a');
 
     var template = jQuery('#location-message-template').html();
-    var html = Mustache.render(template,{
+    var html = Mustache.render(template, {
         from: message.from,
         url: message.url,
         createAt: formattedTime
     });
-    
+
     jQuery('#messages').append(html);
     scrollToBottom();
 })
 
-jQuery("#message-form").on('submit', function(e){
+jQuery("#message-form").on('submit', function (e) {
     e.preventDefault();
 
     var messageTextbox = jQuery('[name=message]');
-    socket.emit('createMessage',{
+    socket.emit('createMessage', {
         from: 'User',
-        text: messageTextbox .val()
-    }, function(){
-        messageTextbox .val('')
-    });    
+        text: messageTextbox.val()
+    }, function () {
+        messageTextbox.val('')
+    });
 })
 
 var locationButton = jQuery('#send-location');
-locationButton.on('click', function(){
-    if(!navigator.geolocation){
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
 
     locationButton.attr('disabled', 'disabled').text('Sending location ..');
 
-    navigator.geolocation.getCurrentPosition(function(position){
+    navigator.geolocation.getCurrentPosition(function (position) {
         locationButton.removeAttr('disabled').text('Send location');
 
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
-    }, function(){
+    }, function () {
         locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     })
